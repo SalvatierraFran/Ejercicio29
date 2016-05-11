cb23813319aa6ca07e86307718cf9f5e52b521d1
SalvatierraFran <franco14_93@hotmail.com>
2016-05-11 01:29:11 -0300

    Ejercicio de propiedades

---
+++ Ejercicio29/Cuenta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    class Cuenta
+    {
+        private string _nombre;
+        private Int64 _nroCuenta;
+        private double _saldo;
+        private Single _tipoInteres;
+
+        /*Constructores*/
+        public Cuenta(string Nombre, Int64 NroCuenta, double Saldo, Single TipoInteres)
+        {
+            this._nombre = Nombre;
+            this._nroCuenta = NroCuenta;
+            this._saldo = Saldo;
+            this._tipoInteres = TipoInteres;
+        }
+
+        /*Propiedades*/
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        public Int64 NroCuenta
+        {
+            get { return this._nroCuenta; }
+            set { this._nroCuenta = value; }
+        }
+
+        public double Saldo
+        {
+            get { return this._saldo; }
+        }
+
+        public Single TipoInteres
+        {
+            get { return this._tipoInteres; }
+            set { this._tipoInteres = value; }
+        }
+
+        /*Metodos*/
+        public void Depositar(Int64 cantidad)
+        {
+            this._saldo += cantidad;
+        }
+
+        public static void Mostrar(Cuenta UnaCuenta)
+        {
+            Console.WriteLine("\nNombre: " + UnaCuenta.Nombre + "\nNro Cuenta: " + UnaCuenta.NroCuenta + "\nSaldo: " + UnaCuenta.Saldo + "\nTipoInteres: " + UnaCuenta.TipoInteres);
+        }
+    }
+}
---
+++ Ejercicio29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta CuentaUno = new Cuenta("Franco", 1111111111, 25000, 25.5f);
+
+            Cuenta.Mostrar(CuentaUno);
+
+            Console.ReadKey();
+        }
+    }
+}
